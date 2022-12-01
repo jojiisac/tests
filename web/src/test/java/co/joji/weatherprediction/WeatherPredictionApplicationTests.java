7f1f2a77a7b59d3ad377780d95fedef8fdf81c5f
Joji Isac <jojiisac@gmail.com>
2022-12-02 00:17:47 +0530

    basic structure

---
+++ web/src/test/java/co/joji/weatherprediction/WeatherPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.joji.weatherprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
